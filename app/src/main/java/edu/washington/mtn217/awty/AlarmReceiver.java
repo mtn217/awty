b7a5d1806bade2582d2233afc37d14d910a63ac9
Michael Nguyen <mtn217@uw.edu>
2017-02-20 21:23:27 -0800

    Added extra credit items

--- app/src/main/java/edu/washington/mtn217/awty/AlarmReceiver.java
+++ app/src/main/java/edu/washington/mtn217/awty/AlarmReceiver.java
@@ -4,6 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
@@ -14,6 +19,20 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String msg = intent.getStringExtra("msg");
         String num = intent.getStringExtra("num");
-        Toast.makeText(context, "(" + num.substring(0, 3) + ") " + num.substring(3, 6) + "-" + num.substring(6) + ": " + msg, Toast.LENGTH_LONG).show();
+//        Toast.makeText(context, "(" + num.substring(0, 3) + ") " + num.substring(3, 6) + "-" + num.substring(6) + ": " + msg, Toast.LENGTH_LONG).show();
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.custom_toast, null);
+
+        TextView message = (TextView) layout.findViewById(R.id.text);
+        TextView text = (TextView) layout.findViewById(R.id.textView3);
+        text.setText("Texting " + "(" + num.substring(0, 3) + ") " + num.substring(3, 6) + "-" + num.substring(6));
+        message.setText(msg);
+
+        Toast toast = new Toast(context);
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
     }
 }
